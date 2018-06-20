b74c861806dbbfdd4ee37497639bec5bc8cb02a0
wenbo <bo.wen@cyzone.cn>
2018-06-20 17:51:22 +0800

    提交图片浏览库

---
+++ 图片浏览/KSPhotoBrowser/KSPhotoBrowser_V0.1.6/KSPhotoBrowser-master/Demo/KSPhotoBrowserDemo/KSDemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  KSDemoViewController.h
+//  KSPhotoBrowser
+//
+//  Created by Kyle Sun on 30/12/2016.
+//  Copyright © 2016 Kyle Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSDemoViewController : UITableViewController
+
+@end
---
+++ 图片浏览/KSPhotoBrowser/KSPhotoBrowser_V0.1.6/KSPhotoBrowser-master/Demo/KSPhotoBrowserDemo/KSPreviewViewController.h
@@ -0,0 +1,27 @@
+//
+//  ViewController.h
+//  KSPhotoBrowser
+//
+//  Created by Kyle Sun on 12/25/16.
+//  Copyright © 2016 Kyle Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "KSPhotoBrowser.h"
+
+typedef NS_ENUM(NSInteger, KSImageManagerType) {
+    KSImageManagerTypeYYWebImage,
+    KSImageManagerTypeSDWebImage
+};
+
+@interface KSPreviewViewController : UIViewController
+
+@property (nonatomic, assign) KSPhotoBrowserInteractiveDismissalStyle dismissalStyle;
+@property (nonatomic, assign) KSPhotoBrowserBackgroundStyle backgroundStyle;
+@property (nonatomic, assign) KSPhotoBrowserPageIndicatorStyle pageindicatorStyle;
+@property (nonatomic, assign) KSPhotoBrowserImageLoadingStyle loadingStyle;
+@property (nonatomic, assign) BOOL bounces;
+@property (nonatomic, assign) KSImageManagerType imageManagerType;
+
+@end
+
