2a7a67417d0cf0856a235e54eb826eb3938e4f43
wenbo <1050794513@qq.com>
2019-10-30 14:28:47 +0800

    添加评分提示封装

---
+++ 提示评分/iRate/iRateHelper/iRateHelper.h
@@ -0,0 +1,21 @@
+//
+//  iRateHelper.h
+//  Start
+//
+//  Created by Mr_Lucky on 2018/9/29.
+//  Copyright © 2018 jmw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface iRateHelper : NSObject
+
++ (instancetype)shareHelper;
+
+- (void)wb_defaultConfig;
+
+@end
+
+NS_ASSUME_NONNULL_END
